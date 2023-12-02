6f6bcd9c82f0d35c32e3a56876aa1629237b50c1
ValeriyKryatov <Kwaler@mail.ru>
2023-12-02 22:53:24 +0300

    SkyproTaskTicket6TechnicalInterviewPart1. First commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,16 @@
 package org.example;
 
 public class Main {
+
+    // Напишите пример перехвата и обработки исключения в блоке try-catch-метода
     public static void main(String[] args) {
 
-        System.out.println("Hello world");
+        try {
+            User.check("safdsdfsd", "akjfhak");
+            System.out.println("Введен корректный логин");
+            System.out.println("Введен корректный пароль");
+        } catch (WrongLoginException | WrongPasswordException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
